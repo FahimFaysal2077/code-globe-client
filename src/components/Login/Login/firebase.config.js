70f705e6ecf90ce14b8b924563bff935d9310311
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-04-16 16:16:14 +0600

    firebase authentication add

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD43OQ8SrrluxOOwu5Y47NKc3tBVL8xfis",
+    authDomain: "code-globe.firebaseapp.com",
+    projectId: "code-globe",
+    storageBucket: "code-globe.appspot.com",
+    messagingSenderId: "1049332228265",
+    appId: "1:1049332228265:web:7c367f02d89e5f92b209b8"
+  };
+
+  export default firebaseConfig;
